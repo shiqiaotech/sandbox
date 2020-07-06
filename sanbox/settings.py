c085fd8e5bb80cf34948925b16fe4fff85df8985
Shiqiao <shiqiaotech@gmail.com>
2020-07-06 00:10:33 -0230

    Use PostgreSQL Database engine

--- sanbox/settings.py
+++ sanbox/settings.py
@@ -75,10 +75,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'sanbox.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_db',
+        'USER': 'ci_user',
+        'PASSWORD': '2020$07%05',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
